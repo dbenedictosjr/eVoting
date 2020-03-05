95010d7d5378ce2f5814f8666e76f61ece6bcfa2
Diego Benedictos Jr <d.t.benedictos.jr@accenture.com>
2020-03-05 10:13:45 +0800

    ddd

---
+++ OSPI.eVoting/wwwroot/images/MemberImage/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OSPI.eVoting.wwwroot.images.MemberImage
+{
+    public class Class
+    {
+    }
+}
